fb9bae3db00ad74e764b8b2ffeb80d3770faa042
Jimmy_MyLaptop <Jimmy@Laptop-EUNDOS1>
2020-11-10 17:26:06 +0800

    修改文件夹名称

--- HikPlayer/Form1.cs
+++ HikPlayer/Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using HikPlayer.HickPlayerSDK;
+using HikPlayer.HikPlayerSDK;
 
 namespace HikPlayer
 {
@@ -23,21 +23,21 @@ namespace HikPlayer
 
         private void btnOpenFile_Click(object sender, EventArgs e)
         {
-            bool openfile = HikPlayer.HickPlayerSDK.HikPlayer.PlayM4_OpenFile(playport,"weightcheck.mp4");
+            bool openfile = HikPlayer.HikPlayerSDK.HikPlayer.PlayM4_OpenFile(playport,"weightcheck.mp4");
             if (openfile == false)
                 MessageBox.Show("打开文件失败");
         }
 
         private void btnPlay_Click(object sender, EventArgs e)
         {
-            bool getport = HikPlayer.HickPlayerSDK.HikPlayer.PlayM4_GetPort(ref playport);
+            bool getport = HikPlayer.HikPlayerSDK.HikPlayer.PlayM4_GetPort(ref playport);
             if (getport == false)
                 MessageBox.Show("获取闲置端口失败");
-            bool openfile = HikPlayer.HickPlayerSDK.HikPlayer.PlayM4_OpenFile(playport, "weightcheck.mp4");
+            bool openfile = HikPlayer.HikPlayerSDK.HikPlayer.PlayM4_OpenFile(playport, "weightcheck.mp4");
             if (openfile == false)
                 MessageBox.Show("打开文件失败");
             IntPtr render_WM = panel1.Handle;
-            bool play=HikPlayer.HickPlayerSDK.HikPlayer.PlayM4_Play(playport, render_WM);
+            bool play=HikPlayer.HikPlayerSDK.HikPlayer.PlayM4_Play(playport, render_WM);
             if (play == false)
                 MessageBox.Show("播放失败");
             else {
@@ -45,22 +45,22 @@ namespace HikPlayer
                 Task.Factory.StartNew(()=> {
                     while (true)
                     {
-                        float pos = HikPlayer.HickPlayerSDK.HikPlayer.PlayM4_GetPlayPos(playport);
+                        float pos = HikPlayer.HikPlayerSDK.HikPlayer.PlayM4_GetPlayPos(playport);
                         this.Invoke((MethodInvoker)delegate {
                             label1.Text = $"当前播放进度:{pos}";
                         });
                         if (pos >= 1)
                         {
-                            bool stop=HikPlayer.HickPlayerSDK.HikPlayer.PlayM4_Stop(playport);
+                            bool stop=HikPlayer.HikPlayerSDK.HikPlayer.PlayM4_Stop(playport);
                             if (stop == false)
                                 MessageBox.Show("停止播放失败!");
 
-                            bool closefile = HikPlayer.HickPlayerSDK.HikPlayer.PlayM4_CloseFile(playport);
+                            bool closefile = HikPlayer.HikPlayerSDK.HikPlayer.PlayM4_CloseFile(playport);
                             if (closefile == false)
                             {
                                 MessageBox.Show("关闭文件失败!");
                             }
-                            bool freeport=HikPlayer.HickPlayerSDK.HikPlayer.PlayM4_FreePort(playport);
+                            bool freeport=HikPlayer.HikPlayerSDK.HikPlayer.PlayM4_FreePort(playport);
                             if (freeport == false)
                             {
                                 MessageBox.Show("释放资源失败!");
--- HikPlayer/HikPlayerSDK/HikPlayer.cs
+++ HikPlayer/HikPlayerSDK/HikPlayer.cs
@@ -5,13 +5,13 @@ using System.Runtime.InteropServices;
 using System.Security;
 using System.Text;
 
-namespace HikPlayer.HickPlayerSDK
+namespace HikPlayer.HikPlayerSDK
 {
     public class HikPlayer
     {
         #region 解码库
         [SecurityCritical]
-        [DllImport("HickPlayerSDK\\PlayCtrl.dll")]
+        [DllImport("HikPlayerSDK\\PlayCtrl.dll")]
         public static extern uint PlayM4_GetLastError(int nPort);
         /// <summary>
         /// 获取文件当前播放位置（百分比）。
@@ -19,7 +19,7 @@ namespace HikPlayer.HickPlayerSDK
         /// <param name="nPort"></param>
         /// <returns></returns>
         [SecurityCritical]
-        [DllImport("HickPlayerSDK\\PlayCtrl.dll")]
+        [DllImport("HikPlayerSDK\\PlayCtrl.dll")]
         public static extern float PlayM4_GetPlayPos(int nPort);
         /// <summary>
         ///设置文件当前播放位置（百分比）。
@@ -27,14 +27,14 @@ namespace HikPlayer.HickPlayerSDK
         /// <param name="nPort"></param>
         /// <returns></returns>
         [SecurityCritical]
-        [DllImport("HickPlayerSDK\\PlayCtrl.dll")]
+        [DllImport("HikPlayerSDK\\PlayCtrl.dll")]
         public static extern float PlayM4_SetPlayPos(int nPort, float fRelativePos);
         /// <summary>
         /// 获取文件大小
         /// </summary>
         /// <returns></returns>
         [SecurityCritical]
-        [DllImport("HickPlayerSDK\\PlayCtrl.dll")]
+        [DllImport("HikPlayerSDK\\PlayCtrl.dll")]
         public static extern uint PlayM4_GetFileHeadLength();
         /// <summary>
         /// 获取文件总时间
@@ -42,7 +42,7 @@ namespace HikPlayer.HickPlayerSDK
         /// <param name="nPort"></param>PlayM4_GetPlayedTime
         /// <returns></returns>
         [SecurityCritical]
-        [DllImport("HickPlayerSDK\\PlayCtrl.dll")]
+        [DllImport("HikPlayerSDK\\PlayCtrl.dll")]
         public static extern uint PlayM4_GetFileTime(int nPort);
         /// <summary>
         /// 获取当前播放时间
@@ -50,17 +50,17 @@ namespace HikPlayer.HickPlayerSDK
         /// <param name="nPort"></param>
         /// <returns></returns>
         [SecurityCritical]
-        [DllImport("HickPlayerSDK\\PlayCtrl.dll")]
+        [DllImport("HikPlayerSDK\\PlayCtrl.dll")]
         public static extern uint PlayM4_GetPlayedTime(int nPort);
         [SecurityCritical]
-        [DllImport("HickPlayerSDK\\PlayCtrl.dll")]
+        [DllImport("HikPlayerSDK\\PlayCtrl.dll")]
         public static extern int PlayM4_GetCaps();
         /// <summary>
         /// 关闭声音
         /// </summary>
         /// <returns></returns>
         [SecurityCritical]
-        [DllImport("HickPlayerSDK\\PlayCtrl.dll")]
+        [DllImport("HikPlayerSDK\\PlayCtrl.dll")]
         public static extern bool PlayM4_StopSound();
         /// <summary>
         /// 获取未使用的通道号
@@ -68,14 +68,14 @@ namespace HikPlayer.HickPlayerSDK
         /// <param name="nPort"></param>
         /// <returns></returns>
         [SecurityCritical]
-        [DllImport("HickPlayerSDK\\PlayCtrl.dll")]
+        [DllImport("HikPlayerSDK\\PlayCtrl.dll")]
         public static extern bool PlayM4_GetPort(ref int nPort);
         /// <summary>
         ///播放声音
         /// </summary>
         /// <param name="nPort"></param>
         [SecurityCritical]
-        [DllImport("HickPlayerSDK\\PlayCtrl.dll")]
+        [DllImport("HikPlayerSDK\\PlayCtrl.dll")]
         public static extern bool PlayM4_PlaySound(int nPort);
         /// <summary>
         /// 设置流播放模式
@@ -84,7 +84,7 @@ namespace HikPlayer.HickPlayerSDK
         /// <param name="mode"></param>
         /// <returns></returns>
         [SecurityCritical]
-        [DllImport("HickPlayerSDK\\PlayCtrl.dll")]
+        [DllImport("HikPlayerSDK\\PlayCtrl.dll")]
         public static extern bool PlayM4_SetStreamOpenMode(int nPort, int mode);
         /// <summary>
         /// 打开流
@@ -95,7 +95,7 @@ namespace HikPlayer.HickPlayerSDK
         /// <param name="nBufPoolSize"></param>
         /// <returns></returns>
         [SecurityCritical]
-        [DllImport("HickPlayerSDK\\PlayCtrl.dll")]
+        [DllImport("HikPlayerSDK\\PlayCtrl.dll")]
         public static extern bool PlayM4_OpenStream(int nPort, byte[] pFileHeadBuf, UInt32 nSize, uint nBufPoolSize);
         /// <summary>
         /// 设置播放缓冲区最大缓冲帧数
@@ -104,7 +104,7 @@ namespace HikPlayer.HickPlayerSDK
         /// <param name="nNum"></param>
         /// <returns></returns>
         [SecurityCritical]
-        [DllImport("HickPlayerSDK\\PlayCtrl.dll")]
+        [DllImport("HikPlayerSDK\\PlayCtrl.dll")]
         public static extern bool PlayM4_SetDisplayBuf(int nPort, uint nBufPoolSize);
         /// <summary>
         /// 打开文件
@@ -113,7 +113,7 @@ namespace HikPlayer.HickPlayerSDK
         /// <param name="hWnd"></param>
         /// <returns></returns>
         [SecurityCritical]
-        [DllImport("HickPlayerSDK\\PlayCtrl.dll")]
+        [DllImport("HikPlayerSDK\\PlayCtrl.dll")]
         public static extern bool PlayM4_OpenFile(int nPort, string fileName);
         /// <summary>
         /// 关闭文件
@@ -122,7 +122,7 @@ namespace HikPlayer.HickPlayerSDK
         /// <param name="hWnd"></param>
         /// <returns></returns>
         [SecurityCritical]
-        [DllImport("HickPlayerSDK\\PlayCtrl.dll")]
+        [DllImport("HikPlayerSDK\\PlayCtrl.dll")]
         public static extern bool PlayM4_CloseFile(int nPort);
         /// <summary>
         /// 开启播放
@@ -131,7 +131,7 @@ namespace HikPlayer.HickPlayerSDK
         /// <param name="hWnd"></param>
         /// <returns></returns>
         [SecurityCritical]
-        [DllImport("HickPlayerSDK\\PlayCtrl.dll")]
+        [DllImport("HikPlayerSDK\\PlayCtrl.dll")]
         public static extern bool PlayM4_Play(int nPort, IntPtr hWnd);
         /// <summary>
         /// 开始倒放
@@ -139,7 +139,7 @@ namespace HikPlayer.HickPlayerSDK
         /// <param name="nPort"></param>
         /// <returns></returns>
         [SecurityCritical]
-        [DllImport("HickPlayerSDK\\PlayCtrl.dll")]
+        [DllImport("HikPlayerSDK\\PlayCtrl.dll")]
         public static extern bool PlayM4_ReversePlay(int nPort);
         /// <summary>
         /// 输入流数据
@@ -149,7 +149,7 @@ namespace HikPlayer.HickPlayerSDK
         /// <param name="nSize"></param>
         /// <returns></returns>
         [SecurityCritical]
-        [DllImport("HickPlayerSDK\\PlayCtrl.dll")]
+        [DllImport("HikPlayerSDK\\PlayCtrl.dll")]
         public static extern bool PlayM4_InputData(int nPort, byte[] pBuf, uint nSize);
         /// <summary>
         /// 关闭播放
@@ -157,7 +157,7 @@ namespace HikPlayer.HickPlayerSDK
         /// <param name="nPort"></param>
         /// <returns></returns>PlayM4_Pause
         [SecurityCritical]
-        [DllImport("HickPlayerSDK\\PlayCtrl.dll")]
+        [DllImport("HikPlayerSDK\\PlayCtrl.dll")]
         public static extern bool PlayM4_Stop(int nPort);
         /// <summary>
         ///暂停/播放1：暂停，0：恢复
@@ -165,7 +165,7 @@ namespace HikPlayer.HickPlayerSDK
         /// <param name="nPort"></param>
         /// <returns></returns>
         [SecurityCritical]
-        [DllImport("HickPlayerSDK\\PlayCtrl.dll")]
+        [DllImport("HikPlayerSDK\\PlayCtrl.dll")]
         public static extern bool PlayM4_Pause(int nPort, uint nPause);
         /// <summary>
         /// 关闭流
@@ -173,7 +173,7 @@ namespace HikPlayer.HickPlayerSDK
         /// <param name="nPort"></param>
         /// <returns></returns>
         [SecurityCritical]
-        [DllImport("HickPlayerSDK\\PlayCtrl.dll")]
+        [DllImport("HikPlayerSDK\\PlayCtrl.dll")]
         public static extern bool PlayM4_CloseStream(int nPort);
         /// <summary>
         /// 释放已使用的通道号
@@ -181,7 +181,7 @@ namespace HikPlayer.HickPlayerSDK
         /// <param name="nPort"></param>
         /// <returns></returns>
         [SecurityCritical]
-        [DllImport("HickPlayerSDK\\PlayCtrl.dll")]
+        [DllImport("HikPlayerSDK\\PlayCtrl.dll")]
         public static extern bool PlayM4_FreePort(int nPort);
 
         #endregion

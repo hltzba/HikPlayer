5fdce3bb5d60c2ee1062efda69ef75994a67be22
Jimmy_MyLaptop <Jimmy@Laptop-EUNDOS1>
2020-11-10 16:42:28 +0800

    初始提交

---
+++ HikPlayer/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HikPlayer.HickPlayerSDK;
+
+namespace HikPlayer
+{
+    public partial class Form1 : Form
+    {
+        int playport = -1;
+        public Form1()
+        {
+            InitializeComponent();
+          
+        }
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            bool openfile = HikPlayer.HickPlayerSDK.HikPlayer.PlayM4_OpenFile(playport,"weightcheck.mp4");
+            if (openfile == false)
+                MessageBox.Show("打开文件失败");
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            bool getport = HikPlayer.HickPlayerSDK.HikPlayer.PlayM4_GetPort(ref playport);
+            if (getport == false)
+                MessageBox.Show("获取闲置端口失败");
+            bool openfile = HikPlayer.HickPlayerSDK.HikPlayer.PlayM4_OpenFile(playport, "weightcheck.mp4");
+            if (openfile == false)
+                MessageBox.Show("打开文件失败");
+            IntPtr render_WM = panel1.Handle;
+            bool play=HikPlayer.HickPlayerSDK.HikPlayer.PlayM4_Play(playport, render_WM);
+            if (play == false)
+                MessageBox.Show("播放失败");
+            else {
+               
+                Task.Factory.StartNew(()=> {
+                    while (true)
+                    {
+                        float pos = HikPlayer.HickPlayerSDK.HikPlayer.PlayM4_GetPlayPos(playport);
+                        this.Invoke((MethodInvoker)delegate {
+                            label1.Text = $"当前播放进度:{pos}";
+                        });
+                        if (pos >= 1)
+                        {
+                            bool stop=HikPlayer.HickPlayerSDK.HikPlayer.PlayM4_Stop(playport);
+                            if (stop == false)
+                                MessageBox.Show("停止播放失败!");
+
+                            bool closefile = HikPlayer.HickPlayerSDK.HikPlayer.PlayM4_CloseFile(playport);
+                            if (closefile == false)
+                            {
+                                MessageBox.Show("关闭文件失败!");
+                            }
+                            bool freeport=HikPlayer.HickPlayerSDK.HikPlayer.PlayM4_FreePort(playport);
+                            if (freeport == false)
+                            {
+                                MessageBox.Show("释放资源失败!");
+                            }
+                            return;
+                        }
+                        
+                        Thread.Sleep(1000);
+                    }
+                });
+            }
+        }
+    }
+}
